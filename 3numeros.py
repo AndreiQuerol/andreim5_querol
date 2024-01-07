6c9dcbb7c556c85b48a1c98eaa7250246d09f7a2
AndreiQuerol <andreiquerol@iesmontsia.org>
2024-01-07 19:47:30 +0100

    Nova versio millorada del programa

--- 3numeros.py
+++ 3numeros.py
@@ -3,7 +3,7 @@ from yogi import read
 a = read(int)
 b = read(int)
 c = read(int)
-if a>b and a>c:print(a)
-elif b>a and b>c:print(b)
-else:print(c)
+if a>b and a>c:print("Es mes grant el numero ",a)
+elif b>a and b>c:print("Es mes grant el numero ",b)
+else:print("Es mes grant el numero ",c)
 
