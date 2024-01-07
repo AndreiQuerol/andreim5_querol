5d0333691fff5c33d70850251cde093b8ef6cd0c
AndreiQuerol <145446124+AndreiQuerol@users.noreply.github.com>
2024-01-07 19:53:59 +0100

    Delete 3numeros.py

--- 3numeros.py
+++
@@ -1,9 +0,0 @@
-from yogi import read
-
-a = read(int)
-b = read(int)
-c = read(int)
-if a>b and a>c:print("Es mes grant el numero ",a)
-elif b>a and b>c:print("Es mes grant el numero ",b)
-else:print("Es mes grant el numero ",c)
-
